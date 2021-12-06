3e99f635e5f64a426f56f74353ddeb818d43fcec
JonatasAndrade <jonatassilveiraandrade@gmail.com>
2021-12-06 15:08:16 -0300

    Basic routes logic added.

--- 2 - Working with Express/deploy.js
+++ 2 - Working with Express/deploy.js
@@ -8,5 +8,15 @@ const express = require('express');
 
 const app = express();
 
-//Deploys server on port 3500
+/** ROUTES **/
+
+app.use('/users', (req, res, next) => {
+    res.send('<html><h2>Registered Users:</h2><ul><li>Dummy123</li><li>JohnDoemmy</li></ul></html>');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<html><h1>Server up and running!<h1></html>');
+});
+
+//Deploys server non port 3500
 app.listen(3500);
