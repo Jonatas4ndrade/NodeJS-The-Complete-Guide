dd709a26c6b5fb889cf2399db90faa3f8509c11f
JonatasAndrade <jonatassilveiraandrade@gmail.com>
2021-12-07 23:28:17 -0300

    body-parser middleware installed. Folders created for segregating routes and HTML pages.

--- 2 - Working with Express/deploy.js
+++ 2 - Working with Express/deploy.js
@@ -3,14 +3,23 @@ const http = require('http');
 
 //Third party
 const express = require('express');
+const bodyParser = require('body-parser');  
 
 /** IMPORTS ABOVE **/
 
+// Defines Express "object"
 const app = express();
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}));
 
 /** ROUTES **/
 
 app.use('/users', (req, res, next) => {
+    res.send('<html><h2>Registered Users:</h2><ul><li>Dummy123</li><li>JohnDoemmy</li></ul></html>');
+});
+
+app.use('/add-product', (req, res, next) => {
     res.send('<html><h2>Registered Users:</h2><ul><li>Dummy123</li><li>JohnDoemmy</li></ul></html>');
 });
 
