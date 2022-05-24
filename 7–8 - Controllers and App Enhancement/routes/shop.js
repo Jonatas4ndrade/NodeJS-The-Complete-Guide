be20a81e4bbeb9d90444c83d2ebc091a252cdf47
JonatasAndrade <jonatassilveiraandrade@gmail.com>
2022-05-23 21:22:25 -0300

    Some new views skeletons added, within their basic routing and controllers.

--- 7–8 - Controllers and App Enhancement/routes/shop.js
+++ 7–8 - Controllers and App Enhancement/routes/shop.js
@@ -5,5 +5,9 @@ const productsController = require('../controllers/shop');
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
+router.get('/home', productsController.getProducts);
+router.get('/cart', productsController.cartGet);
+router.get('/checkout', productsController.checkoutGet);
+router.get('/product-list', productsController.getProducts);
 
 module.exports = router;
